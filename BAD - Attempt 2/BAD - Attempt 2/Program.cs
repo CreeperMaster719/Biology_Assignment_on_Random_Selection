6bd08fead79daae18b2e8e40bf6bc83419d73b00
Eliot Troop <etroop719@gmail.com>
2019-02-27 23:40:58 -0800

    Attempt 1 was a fail and Attempt 2 works like a charm.
    I stayed up 4 hours past my bedtime for this so if my code is sloppy, I apologise.

---
+++ BAD - Attempt 2/BAD - Attempt 2/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAD___Attempt_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is attempt 2 because I absolutely failed attempt 1.
+            Random random = new Random();
+            int[] prey = new int[99];
+            int[] successor = new int[101];
+            int[] endTotal = new int[6];
+            int proceed = 0;
+            int generations = 0;
+            Console.WriteLine("How many generations should pass?");
+            int numberOfGenerations = int.Parse(Console.ReadLine());
+            for (int i = 0; i < prey.Length; i++)
+            {
+                prey[i] += random.Next(1, 6);
+            }
+            for (int i = 0; i < 74; i++)
+            {
+                int tmp = random.Next(0, 99);
+                while (prey[tmp] == 0)
+                {
+                    tmp = random.Next(0, 99);
+                }
+                prey[tmp] = 0;
+            }
+            for (int i = 0; i < 99; i++)
+            {
+
+
+
+                if (prey[i] != 0)
+                {
+
+                    for (int j = ((proceed + 1) * 4) - 4; j < (proceed + 1) * 4; j++)
+                    {
+
+                        successor[j] = prey[i];
+
+                    }
+                    proceed++;
+                }
+
+            }
+            for (int i = 0; i < 100; i++)
+            {
+                endTotal[successor[i]]++;
+            }
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine($"There were {endTotal[i]} prey of species {i} on generation {generations}.");
+                
+            }
+            while (generations != numberOfGenerations)
+                {
+                proceed = 0;
+                Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                for(int i = 0; i < endTotal.Length; i++)
+                {
+                    endTotal[i] = 0;
+                }
+                for(int i = 0; i < 99; i++)
+                {
+                    prey[i] = successor[i];
+                }
+                for(int i = 0; i < 100; i++)
+                {
+                    successor[i] = 0;
+                }
+            for (int i = 0; i < 74; i++)
+            {
+                int tmp = random.Next(0, 99);
+                while (prey[tmp] == 0)
+                {
+                    tmp = random.Next(0, 99);
+                }
+                prey[tmp] = 0;
+            }
+            for (int i = 0; i < 99; i++)
+            {
+
+
+
+                if (prey[i] != 0)
+                {
+
+                    for (int j = ((proceed + 1) * 4) - 4; j < (proceed + 1) * 4; j++)
+                    {
+
+                        successor[j] = prey[i];
+
+                    }
+                    proceed++;
+                }
+
+            }
+            for (int i = 0; i < 100; i++)
+            {
+                endTotal[successor[i]]++;
+            }
+            for (int i = 1; i < 6; i++)
+            {
+                Console.WriteLine($"There were {endTotal[i]} prey of species {i} on generation {generations + 1}.");
+                
+            }
+                generations++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ BiologyAssignmentDiversity/BiologyAssignmentDiversity/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiologyAssignmentDiversity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //All code by Eliot Troop, Project for 9th Grade Honors Biology at Laurel Springs School.
+            Console.WriteLine("Hello User, Please enter the generation of your previous set of data in the form of a number between 0 and 99.");
+            int generation = int.Parse(Console.ReadLine());
+            Console.WriteLine("For each species, write the number of existing ones as they are asked for. (Starting new sets requires putting 20 for each option.");
+            int[] species = new int[5];
+
+                for(int i = 0; i < species.Length; i++)
+            {
+                Console.WriteLine($"Species {i + 1}");
+                species[i] = int.Parse(Console.ReadLine()) ;
+            }
+            int numberLeft = species.Length * 20;
+            Random predators = new Random();
+            Console.WriteLine($"Generation {generation}.");
+            for (int i = 0; i < species.Length; i++)
+            {
+                if(numberLeft > 45 && species[i] >= 20)
+                {
+                    species[i] -= predators.Next(0, 20);
+                    numberLeft -= 20 - species[i];
+                }
+                else if (numberLeft == 20)
+                {
+                    species[i] -= predators.Next(0, (numberLeft - 20));
+                    numberLeft -= 20 - species[i];
+                }
+                else if(numberLeft < 20)
+                {
+                    int tmp = species[i];
+                    species[i] += predators.Next(0, 5);
+                    numberLeft += species[i] - tmp;
+
+                }
+
+            }
+            while (numberLeft != 25)
+            {
+                int tmp0 = predators.Next(0, species.Length);
+                    if (numberLeft > 25 && species[tmp0] > 5)
+                    {
+                        int tmp = species[tmp0];
+                    species[tmp0] -= predators.Next(0, 5);
+                        numberLeft -= tmp - species[tmp0];
+                    }
+                if (species[tmp0] <= 5)
+                {
+                    int tmp = 0;
+                    tmp = species[tmp0];
+                    species[tmp0] += predators.Next(0, 10);
+                    numberLeft += tmp - species[tmp0];
+                }
+
+            }
+
+            for (int i = 0; i < species.Length; i++)
+            {
+                species[i] *= 4;
+                Console.WriteLine($"Species {i + 1} of generation {generation} contains {species[i]} prey.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
